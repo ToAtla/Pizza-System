5a07d715ec7c040a7c2b63fe7380fa0a0cd598be
ToAtla <thorduratl17@ru.is>
2017-11-28 16:54:23 +0000

    Pizza Klasa bætt við

---
+++ PizzaKerfi/PizzaKerfi/Pizza.cpp
@@ -0,0 +1,9 @@
+//
+//  Pizza.cpp
+//  PizzaKerfi
+//
+//  Created by Þórður Atlason on 28/11/2017.
+//  Copyright © 2017 Þórður Atlason. All rights reserved.
+//
+
+#include "Pizza.hpp"
---
+++ PizzaKerfi/PizzaKerfi/Pizza.hpp
@@ -0,0 +1,14 @@
+//
+//  Pizza.hpp
+//  PizzaKerfi
+//
+//  Created by Þórður Atlason on 28/11/2017.
+//  Copyright © 2017 Þórður Atlason. All rights reserved.
+//
+
+#ifndef Pizza_hpp
+#define Pizza_hpp
+
+#include <stdio.h>
+
+#endif /* Pizza_hpp */
