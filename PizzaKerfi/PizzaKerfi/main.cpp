bb8e7653272e6051bca8645d8434d41dc66bdfb6
ToAtla <thorduratl17@ru.is>
2017-11-28 16:15:20 +0000

    Upphaf

---
+++ PizzaKerfi/PizzaKerfi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PizzaKerfi
+//
+//  Created by Þórður Atlason on 28/11/2017.
+//  Copyright © 2017 Þórður Atlason. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
