6f2d569fa36d2244288c8d67357fc8298bd23730
ToAtla <thorduratl17@ru.is>
2017-11-30 10:18:08 +0000

    LocationRepo bætt við

---
+++ PizzaKerfi/PizzaKerfi/LocationRepo.cpp
@@ -0,0 +1,9 @@
+//
+//  LocationRepo.cpp
+//  PizzaKerfi
+//
+//  Created by Þórður Atlason on 30/11/2017.
+//  Copyright © 2017 Þórður Atlason. All rights reserved.
+//
+
+#include "LocationRepo.hpp"
---
+++ PizzaKerfi/PizzaKerfi/LocationRepo.hpp
@@ -0,0 +1,18 @@
+//
+//  LocationRepo.hpp
+//  PizzaKerfi
+//
+//  Created by Þórður Atlason on 30/11/2017.
+//  Copyright © 2017 Þórður Atlason. All rights reserved.
+//
+
+#ifndef LocationRepo_hpp
+#define LocationRepo_hpp
+
+#include <stdio.h>
+
+class LocationRepo{
+    
+};
+
+#endif /* LocationRepo_hpp */
