1858a5c26c02047a031d5569facb0e50d673d788
nokkvikarlsson <nokkvi17@ru.is>
2017-11-30 19:38:29 +0000

    bjó til klasann Side og kommentaði klasann Topping

--- PizzaKerfi/PizzaKerfi/Topping.cpp
+++ PizzaKerfi/PizzaKerfi/Topping.cpp
@@ -15,28 +15,35 @@ Topping::Topping(){
     name[0] = '\0';
 }
 
+//Returns the private variable name.
 char* Topping::getName(){
     return name;
 }
 
+//Take in a character array as a paramaeter and puts it into the private variable name.
 void Topping::setName(char inName[MAXCHARINTOP]){
     for(int i = 0; i < MAXCHARINTOP; i++){
     name[i] = inName[i];
     }
 }
 
+//Returns the price of the topping.
 int Topping::getPrice(){
     return price;
 }
 
+//Takes in a parameter and gives the private variable price the value of the parameter
 void Topping::setPrice(int inPrice){
     price = inPrice;
 }
 
+//Takes in a bool parameter and gives the private variable verbose the value of the parameter.
 void Topping::setVerbose(bool v){
     verbose = v;
 }
 
+
+//Overloads the ostream operator so one can use the '<<' operator to print the contents of a topping instance.
 ostream& operator << (ostream& out, const Topping& topping){
     if(topping.verbose)
         out << "Topping: ";
@@ -46,11 +53,12 @@ ostream& operator << (ostream& out, const Topping& topping){
         out << "Price: ";
     out << topping.price << endl;
     
-    
     return out;
 }
 
+//Overloads the ostream operator so one can use the '>>' operator to input the contents of a topping instance.
 istream& operator >> (istream& in, Topping& topping){
+    
     if(topping.verbose)
         cout << "Enter topping name: ";
     in >> topping.name;
@@ -58,5 +66,6 @@ istream& operator >> (istream& in, Topping& topping){
     if(topping.verbose)
         cout << "Enter topping price: ";
     in >> topping.price;
+    
     return in;
 }
---
+++ PizzaKerfi/Side.hpp
@@ -0,0 +1,40 @@
+//
+//  Side.hpp
+//  PizzaKerfi
+//
+//  Created by Nokkvi Karlsson on 11/30/17.
+//  Copyright © 2017 Þórður Atlason. All rights reserved.
+//
+
+#ifndef Side_hpp
+#define Side_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+const int MAXCHARINSIDE = 30;
+
+class Side{
+
+public:
+    Side();
+    int getPrice();
+    void setPrice(int inPrice);
+    void setVerbose(bool v);
+    char* getName();
+    void setName(char inName[]);
+    friend istream& operator >> (istream& in, Side& side);
+    friend ostream& operator << (ostream& out, const Side& side);
+private:
+    char name[MAXCHARINSIDE];
+    int price;
+    bool verbose = true;
+    
+};
+
+
+
+#endif /* Side_hpp */
