cab56d3e45d1ad2408e3e14226e2c3e58e8c4109
nokkvikarlsson <nokkvi17@ru.is>
2017-11-30 21:16:57 +0000

    Útfærði SideRepo cpp skránna og kommentaði hana

--- PizzaKerfi/PizzaKerfi/SideRepo.cpp
+++ PizzaKerfi/PizzaKerfi/SideRepo.cpp
@@ -7,3 +7,59 @@
 //
 
 #include "SideRepo.hpp"
+
+//Færibreytulaus smiður.
+SideRepo::SideRepo(){
+    
+}
+
+//Bætir breytu af taginu Side inní textaskránna "toppings.txt"
+void SideRepo::addSide(Side& side){
+    ofstream fout;
+    fout.open("sides.txt", ios::app);
+    side.setVerbose(false);
+    fout << side;
+    fout.close();
+    side.setVerbose(true);
+}
+
+//Skilar vector af öllum meðlætum sem eru í "sides.txt"
+vector<Side> SideRepo::getVectorOfSides(){
+    
+    vector<Side> sides;
+    ifstream fin;
+    fin.open("sides.txt");
+    while(!fin.eof()){
+        Side temp;
+        temp.setVerbose(false);
+        fin >> temp;
+        temp.setVerbose(true);
+        sides.push_back(temp);
+    }
+    sides.pop_back();
+    return sides;
+}
+
+//Byrjar á því að tæma textaskránna "sides.txt"
+//Tekur inn vector af sides og setur það inn í textaskránna.
+void SideRepo::storeVectorOfToppings(vector<Side> sides){
+    
+    clearSides();
+    
+    ofstream fout;
+    fout.open("sides.txt");
+    for(int i = 0; i < sides.size(); i++){
+        sides[i].setVerbose(false);
+        fout << sides[i];
+    }
+    fout.close();
+}
+
+
+//Tæmir textaskránna "sides.txt"
+void SideRepo::clearSides()
+{
+    ofstream fout;
+    fout.open("sides.txt");
+    fout.close();
+}
