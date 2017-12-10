e63520a52b551a380e63b5ffbbc474cd70e6848f
ToAtla <thorduratl17@ru.is>
2017-12-10 12:11:03 +0000

    Allt að mjakast með pantarir

--- PizzaKerfi/PizzaKerfi/OrderRepo.cpp
+++ PizzaKerfi/PizzaKerfi/OrderRepo.cpp
@@ -14,27 +14,29 @@ OrderRepo::OrderRepo(){
 
 void OrderRepo::storeOrder(const Order& order){
     ofstream fout;
-    fout.open("orders.dat", ios::binary|ios::app);
+    fout.open(ORDERFILE, ios::binary|ios::app);
     fout.write((char*)(&order), sizeof(Order));
     fout.close();
 }
 
-Order OrderRepo::retrieveOrder(){
-    Order order;
-    
-    return order;
-}
 
 Order* OrderRepo::retrieveOrderArray(string fileName, int& tellMeHowManyOrders){
-    Order* orderList = new Order[MAXSTUFFSINORDER];
+    
     ifstream fin;
     fin.open(fileName, ios::binary);
-    fin.seekg(0, fin.end);
-    tellMeHowManyOrders = (int)(fin.tellg() / sizeof(Order));
-    fin.seekg(0, fin.beg);
-    fin.read((char*)(orderList), sizeof(Order)*tellMeHowManyOrders);
-    fin.close();
-    return orderList;
+    if (fin.is_open()) {
+        fin.seekg(0, fin.end);
+        tellMeHowManyOrders = (int)(fin.tellg() / sizeof(Order));
+        fin.seekg(0, fin.beg);
+        Order* orderList = new Order[tellMeHowManyOrders];
+        fin.read((char*)(orderList), sizeof(Order)*tellMeHowManyOrders);
+        fin.close();
+        return orderList;
+    }else{
+        tellMeHowManyOrders = 0;
+        Order* orderList = NULL;
+        return orderList;
+    }
 }
 
 bool OrderRepo::fileExists(string fileName){
