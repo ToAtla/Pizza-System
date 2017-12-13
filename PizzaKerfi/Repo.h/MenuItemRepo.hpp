1249ca4b0e8eb2ce799260082b3b522edc14bfc1
nokkvikarlsson <nokkvi17@ru.is>
2017-12-13 17:30:15 +0000

    MenuItem að byrja

---
+++ PizzaKerfi/Repo.h/MenuItemRepo.hpp
@@ -0,0 +1,33 @@
+//
+//  MenuItemRepo.hpp
+//  PizzaKerfi
+//
+//  Created by Nokkvi Karlsson on 12/13/17.
+//  Copyright © 2017 Þórður Atlason. All rights reserved.
+//
+
+#ifndef MenuItemRepo_hpp
+#define MenuItemRepo_hpp
+
+#include <stdio.h>
+
+#include <vector>
+#include "MenuItem.hpp"
+#include "Global.hpp"
+
+using namespace std;
+
+class MenuItemRepo{
+    
+public:
+    void addMenuItem(MenuItem& MenuItem);
+    void displayMenuItem();
+    MenuItem* getVectorOfMenuItems();
+    void storeVectorOfMenuItems(vector<MenuItem> MenuItems);
+private:
+    void clearMenuItems();
+    void createMenuItem();
+};
+
+
+#endif /* MenuItemRepo_hpp */
---
+++ PizzaKerfi/Repo/MenuItemRepo.cpp
@@ -0,0 +1,9 @@
+//
+//  MenuItemRepo.cpp
+//  PizzaKerfi
+//
+//  Created by Nokkvi Karlsson on 12/13/17.
+//  Copyright © 2017 Þórður Atlason. All rights reserved.
+//
+
+#include "MenuItemRepo.hpp"
