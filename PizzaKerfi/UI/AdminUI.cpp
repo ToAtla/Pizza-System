d4a3e848be1e35344c11aa16f9e7f3dfbdd78da7
AndresHardarson <andres16@ru.is>
2017-12-14 18:10:24 +0000

    manageOffers fall í adminui
    manageOffers fall í adminui

--- PizzaKerfi/UI/AdminUI.cpp
+++ PizzaKerfi/UI/AdminUI.cpp
@@ -75,6 +75,10 @@ void AdminUI::startAdminUI(){
             magic.clearScreen();
             displayAnalysis();
         }
+        else if(input == '9'){
+            magic.clearScreen();
+            manageOffers();
+        }
     }
 }
 
@@ -1362,6 +1366,10 @@ void AdminUI::displayAnalysis(){
     uiItemSeparator();
     cout << endl << setfill(CHARFORSPACE) << setw(SIZEFORSETWSPACE) << " " <<"Enter anything to continue: ";
     cin >> ws;
+}
+
+void AdminUI::manageOffers(){
+    
 }
 
 void AdminUI::uiItemSeparator () {
