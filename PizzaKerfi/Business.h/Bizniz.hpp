dc049b40e6095ce895c46642c9f11d13cb32b07d
ToAtla <thorduratl17@ru.is>
2017-12-14 14:18:16 +0000

    Hann kennir pizzur

--- PizzaKerfi/Business.h/Bizniz.hpp
+++ PizzaKerfi/Business.h/Bizniz.hpp
@@ -68,7 +68,7 @@ public:
     //MenuItem
     MenuItem* getArrayOfMenuItems(string fileName, int& tellMeHowMany);
     void addMenuItem(MenuItem& menuItem);
-    MenuItem recognizeMenuItem(Topping* pizzaToppings, int toppingCount, bool isMenuitem);
+    MenuItem recognizeMenuItem(Topping* pizzaToppings, int toppingCount, bool& isMenuitem);
     
     //Analysis
     int calcTotalTurnover();
